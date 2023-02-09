5a023c3986f0620e8134f908336774502aacf2cf
spalacioj <samu43221@gmail.com>
2023-02-09 10:48:19 -0500

    agregando pagina about

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Samuel Palacio'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to about page</h1>')
